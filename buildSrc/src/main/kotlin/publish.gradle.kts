c5296ba95090d038f170bc09e3d6077de8fed66d
GitHub Action <action@github.com>
2021-03-03 09:13:51 +0000

    Template cleanup

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -93,7 +93,7 @@ afterEvaluate {
                         version = "$version-SNAPSHOT"
                     }
                     description.set("A template for Kotlin Android projects")
-                    url.set("https://github.com/cortinico/kotlin-android-template/")
+                    url.set("https://github.com/meshnesh/androidkotlinboilerplate/")
 
                     licenses {
                         license {
@@ -103,18 +103,18 @@ afterEvaluate {
                     }
                     developers {
                         developer {
-                            id.set("cortinico")
-                            name.set("Nicola Corti")
+                            id.set("meshnesh")
+                            name.set("meshnesh")
                         }
                     }
                     scm {
-                        connection.set("scm:git:git://github.com/cortinico/kotlin-android-template.git")
-                        developerConnection.set("scm:git:ssh://github.com/cortinico/kotlin-android-template.git")
-                        url.set("https://github.com/cortinico/kotlin-android-template/")
+                        connection.set("scm:git:git://github.com/meshnesh/androidkotlinboilerplate.git")
+                        developerConnection.set("scm:git:ssh://github.com/meshnesh/androidkotlinboilerplate.git")
+                        url.set("https://github.com/meshnesh/androidkotlinboilerplate/")
                     }
                     issueManagement {
                         system.set("GitHub Issues")
-                        url.set("https://github.com/cortinico/kotlin-android-template/issues")
+                        url.set("https://github.com/meshnesh/androidkotlinboilerplate/issues")
                     }
                 }
             }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
     }
 }
 
-rootProject.name = ("kotlin-android-template")
+rootProject.name = ("androidkotlinboilerplate")
 
 include(
     "app",
